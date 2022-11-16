a5ce899ab1e15563cba50c36e013c9d5a6abdc96
tran thanh tu <tttuab@gmail.com>
2022-11-16 10:58:32 +0700

    update 2

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,63 +1,62 @@
-const ErrorHandler = require('../utils/errorHandler');
+const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
 
-    err.statusCode = err.statusCode || 500;
-
-    if (process.env.NODE_ENV === 'DEVELOPMENT') {
-        console.log(err);
-
-        res.status(err.statusCode).json({
-            success: false,
-            error: err,
-            errMessage: err.message,
-            stack: err.stack
-        })
-    }
-    if (process.env.NODE_ENV === 'PRODUCTION') {
-        let error = { ...err }
-
-        error.message = err.message;
-
-        // Wrong Mongoose Object ID Error
-        // Lỗi ID đối tượng Mongoose sai
-        if (err.name === 'CastError') {
-            const message = `Không tìm thấy tài nguyên. Không hợp lệ: ${err.path}`
-            error = new ErrorHandler(message, 400)
-        }
-        // Handling Mongoose Validation Error
-        // Xử lý lỗi xác thực Mongoose
-        if (err.name === 'ValidationError') {
-            const message = Object.values(err.errors).map(value => value.message);
-            error = new ErrorHandler(message, 400)
-        }
-        // Handling Mongoose duplicate key errors
-        // Xử lý lỗi khóa trùng lặp Mongoose
-        if (err.code === 11000) {
-            const message = `Duplicate ${Object.keys(err.keyValue)} entered`
-            error = new ErrorHandler(message, 400)
-        }
-
-        // // Handling Expired JWT error
-        // Xử lý lỗi JWT hết hạn
-        if (err.name === 'TokenExpiredError') {
-            const message = 'JSON Web Token is expired. Try Again!!!'
-            error = new ErrorHandler(message, 400)
-        }
-
-        res.status(err.statusCode).json({
-            success: false,
-            message: err.message || 'Lỗi máy chủ nội bộ'
-        })
-
-    }
-
-
-
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    console.log(err);
 
     res.status(err.statusCode).json({
-        success: false,
-        error: err.stack
+      success: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    let error = { ...err };
 
-    })
-}
+    error.message = err.message;
+
+    // Wrong Mongoose Object ID Error
+    // Lỗi ID đối tượng Mongoose sai
+    if (err.name === "CastError") {
+      const message = `Không tìm thấy tài nguyên. Không hợp lệ: ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+    // Handling Mongoose Validation Error
+    // Xử lý lỗi xác thực Mongoose
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+    // Handling Mongoose duplicate key errors
+    // Xử lý lỗi khóa trùng lặp Mongoose
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+    // Handling wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+      const message = "JSON Web Token is invalid. Try Again!!!";
+      error = new ErrorHandler(message, 400);
+    }
+
+    // // Handling Expired JWT error
+    // Xử lý lỗi JWT hết hạn
+    if (err.name === "TokenExpiredError") {
+      const message = "JSON Web Token is expired. Try Again!!!";
+      error = new ErrorHandler(message, 400);
+    }
+
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message || "Lỗi máy chủ nội bộ",
+    });
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    error: err.stack,
+  });
+};
--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-const app = require('./app')
-const connectDatabase = require('./config/database')
+const app = require("./app");
+const connectDatabase = require("./config/database");
 
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 const cloudinary = require("cloudinary").v2;
 
-
 // Handle Uncaught exceptions
-process.on('uncaughtException', err => {
-    console.log(`ERROR: ${err.stack}`);
-    console.log('Shutting down due to uncaught exception');
-    process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
 
-dotenv.config({ path: '../server/config/config.env' })
+// dotenv.config({ path: '../server/config/config.env' })
+// Setting up config file
+if (process.env.NODE_ENV !== "PRODUCTION")
+  require("dotenv").config({ path: "../server/config/config.env" });
 
 //Connecting to datatbase
 connectDatabase();
 
 //Setting up cloudinary configuration
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
-
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const PORT = process.env.PORT || 7000;
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server started on PORT: ${PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
 
 // Handle Unhandled Promise rejections
-process.on('unhandledRejection', err => {
-    console.log(`ERROR: ${err.stack}`);
-    console.log('Shutting down the server due to Unhandled Promise rejection');
-    server.close(() => {
-        process.exit(1)
-    })
-})
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down the server due to Unhandled Promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
