cd6025f3fb0a89a0767b6d45bc8d72adae09902c
tran thanh tu <tttuab@gmail.com>
2022-11-29 20:21:22 +0700

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -21,7 +21,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     }
 
     // const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    const decoded = jwt.verify(token, JWT_SECRET)
+    const decoded = jwt.verify(token, 'UEIH4353R4234E09NFDJHDREERRE34FWG2M4KFLS')
     req.user = await User.findById(decoded.id);
 
     next()
