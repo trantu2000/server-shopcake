d4390c68f5f7513f9d7e68e9d0636e84c2894711
tran thanh tu <tttuab@gmail.com>
2022-11-29 19:42:56 +0700

    update

--- models/user.js
+++ models/user.js
@@ -73,20 +73,20 @@ userSchema.methods.getJwtToken = function () {
   };
 
 // Generate password reset token
-userSchema.methods.getResetPasswordToken = function () {
-  // Generate token
-  const resetToken = crypto.randomBytes(20).toString("hex");
+// userSchema.methods.getResetPasswordToken = function () {
+//   // Generate token
+//   const resetToken = crypto.randomBytes(20).toString("hex");
 
-  // Hash and set to resetPasswordToken
-  this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+//   // Hash and set to resetPasswordToken
+//   this.resetPasswordToken = crypto
+//     .createHash("sha256")
+//     .update(resetToken)
+//     .digest("hex");
 
-  // Set token expire time
-  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+//   // Set token expire time
+//   this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
 
-  return resetToken;
-};
+//   return resetToken;
+// };
 
 module.exports = mongoose.model("User", userSchema);
