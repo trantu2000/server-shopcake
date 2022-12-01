2342baf82552cb46abfc4e5f381a0a67f8985279
tran thanh tu <tttuab@gmail.com>
2022-12-02 00:52:22 +0700

    update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,15 +5,15 @@ const sendToken = (user, statusCode, res) => {
     const token = user.getJwtToken()
 
     // Options for cookie
-    // const options = {
-    //     expires: new Date(
-    //         Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-    //     ),
-    //     httpOnly: true
-    // }
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
 
 
-    res.status(statusCode).cookie('token', token).json({
+    res.status(statusCode).cookie('token', token, options).json({
         success: true,
         token,
         user
