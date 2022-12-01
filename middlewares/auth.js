866d16c50af6ed0b5432c854c834705706469605
tran thanh tu <tttuab@gmail.com>
2022-12-02 00:34:19 +0700

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
 
+
 //Kiểm tra xem người dùng có được xác thực hay không
 // Checks if user is authenticated or not
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
