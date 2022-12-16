f6645cbd957584a431bf26261fa3862c832e00c6
tran thanh tu <tttuab@gmail.com>
2022-12-16 23:56:12 +0700

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,13 +8,12 @@ const catchAsyncErrors = require("./catchAsyncErrors");
 // Checks if user is authenticated or not
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
- 
 
   if (!token) {
     return next(new ErrorHandler("Vui lòng dăng nhập", 401));
   }
 
-  const decoded = jwt.verify(token, "UEIH4353R4234E09NFDJ34FWG2MNM");
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
   req.user = await User.findById(decoded.id);
 
   next();
