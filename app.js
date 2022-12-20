d71d444ae72fb91fa4cb61c93e299cf56e33ac92
tran thanh tu <tttuab@gmail.com>
2022-12-20 21:43:09 +0700

    update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const cookieParser = require('cookie-parser')
-const cors = require('cors')
+// const cors = require('cors')
 const bodyParser = require('body-parser')
 
 
 
 const app = express();
-app.use(cors())
+// app.use(cors())
 
 
 
