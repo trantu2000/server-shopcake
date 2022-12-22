45f1a030987dc7797850cf3a7d62653eae01e467
tran thanh tu <tttuab@gmail.com>
2022-12-22 23:00:45 +0700

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 // }))
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://tucake.vercel.app/api/v1");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
