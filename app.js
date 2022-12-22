04223a440390eca64bee542cbc1026a2205350d6
tran thanh tu <tttuab@gmail.com>
2022-12-22 21:25:51 +0700

    update

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ app.use(cors())
 
 
 app.use(express.json({limit:"300mb"}));
-// app.use(bodyParser.urlencoded({ extended: true ,limit:"300mb"}));
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true ,limit:"300mb"}));
 app.use(bodyParser.json());
 app.use(cookieParser())
 
