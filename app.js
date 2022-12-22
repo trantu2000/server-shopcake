0f308d542732a7c2aee8d85741ba27f7fa07f175
tran thanh tu <tttuab@gmail.com>
2022-12-22 22:13:20 +0700

    update

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 app.use(express.json({limit:"300mb"}));
-app.use(bodyParser.urlencoded({ extended: true ,limit:"10000mb"}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser())
 
