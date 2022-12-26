81125dd762e361ee2dc39f1f75dbde4cd5b61e06
tran thanh tu <tttuab@gmail.com>
2022-12-26 21:05:40 +0700

    update

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const app = express();
 
-// app.use(cors({
-//     origin:"https://tucake.vercel.app"
-// }))
+app.use(cors({
+    origin:"https://tucake.vercel.app"
+}))
 
 app.use(express.json({ limit: "300mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 const errorMiddleware = require("./middlewares/errors");
 
