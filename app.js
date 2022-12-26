711a11a3b8c599042fe2425218880c6c71bc0dd9
tran thanh tu <tttuab@gmail.com>
2022-12-26 21:40:52 +0700

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
 
